6b38a3011e1bcf88078289f0492505f15c8c4281
iareanidiot384 <shaunleochan2003@gmail.com>
2024-05-02 20:27:09 +0800

    AlphaRev3

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -8,23 +8,40 @@ public class PlayerHealth : MonoBehaviour
     public int currentHP;
     public HealthBar healthBar;
 
+    private int previousSceneIndex;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHP = maxHP;
         healthBar.SetMaxHP(maxHP);
+        previousSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHP <= 0)
+        {
+            PlayerPrefs.SetString("PreviousScene", SceneManager.GetActiveScene().name);
+            GameOver();
+        }
     }
 
-    public void updateHP(int value)
+    public void UpdateHP(int value)
     {
         currentHP += value;
         healthBar.UpdateHealthBar(currentHP);
         Debug.Log(currentHP + "/" + maxHP);
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(previousSceneIndex);
     }
 }
